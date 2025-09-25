a94022e3dcc70ff2dfba53f21c13b47e802ff1a7
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2025-09-25 09:14:50 -0500

    Change added in order no make the test fail and see the proccess

--- tasks/task1.py
+++ tasks/task1.py
@@ -6,4 +6,4 @@ class Dictionary:
         self.entries[word] = definition
 
     def look(self, word):
-        return self.entries.get(word, f"Can't find entry for {word}")
+        return self.entries.get(word, f"Can' find entry for {word}")
